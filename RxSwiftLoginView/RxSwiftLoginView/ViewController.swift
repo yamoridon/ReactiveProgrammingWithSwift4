c175dc01dba0abc554f991df700a363e78309cbf
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-03-02 03:43:37 +0900

    make login screen with IB

--- RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
+++ RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
@@ -1,4 +1,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var validationsLabel: UILabel!
 }
