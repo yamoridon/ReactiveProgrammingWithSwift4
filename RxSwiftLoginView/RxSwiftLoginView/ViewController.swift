15b952171671222298c43133eac8d948ebcb6b6c
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-03-02 03:24:51 +0900

    fix warnings from SwiftLint

--- RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
+++ RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
@@ -1,20 +1,4 @@
-//
-//  ViewController.swift
-//  RxSwiftLoginView
-//
-//  Created by Kazuki Ohara on 2019/03/02.
-//  Copyright © 2019 Kazuki Ohara. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
-
