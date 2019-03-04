91aeea2e7392a21656273767f26b9d0dded68da0
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-03-04 17:27:48 +0900

    fix warnings from SwiftLint

--- RxBasic/RxBasic/ViewController.swift
+++ RxBasic/RxBasic/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  RxBasic
-//
-//  Created by Kazuki Ohara on 2019/03/04.
-//  Copyright © 2019 Kazuki Ohara. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
