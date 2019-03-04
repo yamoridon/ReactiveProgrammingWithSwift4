ac9e156389faf888323cd485e51b8c57f3dece8d
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-03-04 12:01:47 +0900

    finished chapter 3

--- RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
+++ RxSwiftLoginView/RxSwiftLoginView/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loginButton.isEnabled = false
+        loginButton.backgroundColor = .lightGray
+
         _ = usernameTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.username)
         _ = passwordTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.password)
+
+        _ = loginViewModel.isValid.bind(to: loginButton.rx.isEnabled)
+
+        _ = loginViewModel.isValid.subscribe(onNext: { [unowned self] isValid in
+            self.validationsLabel.text = isValid ? "Enabled" : "Disabled"
+            self.validationsLabel.textColor = isValid ? .green : .red
+
+            // Log the values with each key in event
+            print(isValid)
+
+            // Making changes to the login button
+            self.loginButton.isEnabled = isValid
+            self.loginButton.backgroundColor = isValid ? .orange : .lightGray
+        })
     }
 }
