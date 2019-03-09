3d343e5bf15622b1abaebb09a2884bcee235a15c
Kazuki Ohara <kazuki.ohara@gmail.com>
2019-03-09 13:28:13 +0900

    completed chapter 5

--- RxOperators/RxOperators.playground/Contents.swift
+++ RxOperators/RxOperators.playground/Contents.swift
@@ -35,3 +35,62 @@ executeProcedure(for: "flatMap and flatMapLatest") {
 
     alex.playerScore.accept(96)
 }
+
+executeProcedure(for: "scan and buffer") {
+    let disposeBag = DisposeBag()
+    let gameScore = PublishSubject<Int>()
+
+    gameScore
+        .buffer(timeSpan: 0.0, count: 3, scheduler: MainScheduler.instance)
+        .map {
+            print($0, "--> ", terminator: "")
+            return $0.reduce(0, +)
+        }
+        .scan(501, accumulator: -)
+        .map { max($0, 0) }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+
+    gameScore.onNext(60)
+    gameScore.onNext(13)
+    gameScore.onNext(50)
+}
+
+executeProcedure(for: "filter") {
+    let disposeBag = DisposeBag()
+    let integers = Observable
+        .generate(initialState: 1, condition: { $0 < 101 }, iterate: { $0 + 1 })
+
+    integers
+        .filter { $0.isPrimse() }
+        .toArray()
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+}
+
+executeProcedure(for: "distinctUntilChanged") {
+    let disposeBag = DisposeBag()
+    let stringToSearch = BehaviorRelay(value: "")
+
+    stringToSearch
+        .asObservable()
+        .map { $0.lowercased() }
+        .distinctUntilChanged()
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+
+    stringToSearch.accept("TINTIN")
+    stringToSearch.accept("tintin")
+    stringToSearch.accept("noDDy")
+    stringToSearch.accept("TINTIN")
+}
+
+executeProcedure(for: "takeWhile") {
+    let disposeBag = DisposeBag()
+    let integers = Observable.of(10, 20, 30, 40, 30, 20, 10)
+
+    integers
+        .takeWhile { $0 < 40 }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
--- RxOperators/RxOperators.playground/Sources/Helper.swift
+++ RxOperators/RxOperators.playground/Sources/Helper.swift
@@ -5,3 +5,18 @@ public func executeProcedure(for description: String, procedure: () -> Void) {
     print("Procedure execute for:", description)
     procedure()
 }
+
+public extension Int {
+    func isPrimse() -> Bool {
+        guard self > 1 else { return false }
+
+        var isPrimeFlag = true
+
+        for index in 2..<self {
+            if self % index == 0 {
+                isPrimeFlag = false
+            }
+        }
+        return isPrimeFlag
+    }
+}
